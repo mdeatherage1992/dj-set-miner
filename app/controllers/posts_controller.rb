7c6392e705b86a2991594d7bd9752fd6491b5e83
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-23 10:02:36 -0400

    editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,7 @@ class PostsController < ApplicationController
     @post.downvote_by current_user
     redirect_to :back
   end
-  # GET /posts
-  # GET /posts.json
+
   def index
     @posts = Post.all
     @users = User.all
@@ -36,12 +35,11 @@ class PostsController < ApplicationController
     end
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
+
   def show
   end
 
-  # GET /posts/new
+
   def new
     @post = Post.new
     @post.genres.build
@@ -66,8 +64,7 @@ respond_to do |format|
   end
 end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
+
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -80,8 +77,7 @@ end
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+
   def destroy
     @post.destroy
     respond_to do |format|
@@ -91,12 +87,12 @@ end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    
     def post_params
       params.require(:post).permit(:title, :url, :comment, :video, genre_ids:[], genres_attributes: [:name] )
     end
